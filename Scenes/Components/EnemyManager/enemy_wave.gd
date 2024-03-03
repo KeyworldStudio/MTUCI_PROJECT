05f05ce45b084d8dd89784ec55a4d5ce05514876
Андрей Никитин <146591318+AndrewNikkk@users.noreply.github.com>
2024-03-03 16:26:21 +0300

    untested-wave-manager

---
+++ Scenes/Components/EnemyManager/enemy_wave.gd
@@ -0,0 +1,10 @@
+class_name EnemyWave
+
+extends Resource
+
+@export var wave_scene: PackedScene
+@export var wave_time: float = 60
+@export var enemy_count: int
+
+
+
--- Scenes/Components/enemy_wave.gd
+++
@@ -1,7 +0,0 @@
-class_name EnemyWave
-
-extends Resource
-
-@export var wave_scenes: PackedScene
-@export var wave_timer: float = 60
-@export var enemy_count: int
