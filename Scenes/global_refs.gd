2c6c7e9d7f58bdbe7ac9f0571c981682147bd6eb
ZedManul <zedthemanul@gmail.com>
2024-03-10 14:37:49 +0300

    Made the main scene properly referencable

---
+++ Scenes/global_refs.gd
@@ -0,0 +1,10 @@
+extends Node
+
+var player_node: PlayerController
+var game_world_node: Node2D
+var bullet_holder: Node2D
+var turret_holder: Node2D
+var enemy_holder: Node2D
+var gui_holder: Control
+
+
