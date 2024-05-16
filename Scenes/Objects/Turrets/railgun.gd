f3ea8ee425935a498b5a9a27e378c07f153cd810
unknown <razzhivin546@gmail.com>
2024-05-16 19:50:37 +0300

    alot

--- Scenes/Objects/Turrets/railgun.gd
+++ Scenes/Objects/Turrets/railgun.gd
@@ -1,11 +1,13 @@
-extends Node2D
+extends RayCast2D
 
+@export var sprite: Sprite2D
+@export var hurtbox: Node2D
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
 	pass
+
+func _on_timer_timeout():
+	enabled = true
+
+func _on_timer_2_timeout():
+	enabled = false
